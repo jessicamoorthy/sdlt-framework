267b2cd99804cb6ba4252a204993513de4e8dffe
Roopam <roopamjain@catalyst.net.nz>
2021-04-27 23:02:43 +1200

    MINOR: fix empty string value

--- src/Model/QuestionnaireSubmission.php
+++ src/Model/QuestionnaireSubmission.php
@@ -2441,7 +2441,7 @@ class QuestionnaireSubmission extends DataObject implements ScaffoldingProvider
             return false;
         }
 
-        if (strstr($req->getBody(), $this->ApprovalLinkToken)) {
+        if ($this->ApprovalLinkToken && strstr($req->getBody(), $this->ApprovalLinkToken)) {
             return true;
         }
 
